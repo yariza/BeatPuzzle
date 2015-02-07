f5e5ff496293dc1dc75e0b8d0fd0fbffe71037f9
Yujin Ariza <yujin.ariza@gmail.com>
2015-02-07 02:24:03 -0500

    quick and dirty color manager

---
+++ Assets/Scripts/ColorManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorManager : Singleton<ColorManager> {
+
+    Color GetColorForIndex(int index) {
+        switch (index) {
+            case 0: return Color.cyan;
+            case 1: return Color.red;
+            case 2: return Color.green;
+            case 3: return Color.yellow;
+            default: return Color.white;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
