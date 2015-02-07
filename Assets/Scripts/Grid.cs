ccd4d317abaaa6cc1b6b3f8226cba3371444d18d
Jeffrey Tao <jat2164@columbia.edu>
2015-02-06 21:17:31 -0500

    Starting grid scripts

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grid : MonoBehaviour {
+
+	public GameObject tilePrefab;
+	public int sizeX, sizeY;
+	public int tileScaleX, tileScaleY;
+
+	Tile[,] tiles;
+
+	void Start() {
+		// Initialize a grid of tiles
+		tiles = new Tile[sizeX, sizeY];
+
+		for (int x = 0; x < sizeX; x++) {
+			for (int y = 0; y < sizeY; y++) {
+				GameObject gObject = Instantiate(tilePrefab) as GameObject;
+				Transform trans = gObject.GetComponent<Transform>();
+				trans.parent = transform;
+				trans.scale.set(tileScaleX, tileScaleY);
+				trans.position.set(x * tileScaleX, y * tileScaleY);
+				tiles[x, y] = gObject.GetComponent<Tile>();
+			}
+		}
+	}
+
+	void Update() {
+
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+	enum Type {REFLECT, PASS, ONE_WAY}
+	enum Orientation {N, NE, E, SE, S, SW, W, NW};
+
+	Type type;
+	Orientation orientation;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
