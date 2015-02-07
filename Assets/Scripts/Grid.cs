683ae48718b5aadd77112834f68b279fb05a0746
Jeffrey Tao <jat2164@columbia.edu>
2015-02-06 23:11:45 -0500

    Drawing grid lines

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -6,6 +6,7 @@ public class Grid : MonoBehaviour {
 	public GameObject reflectPrefab;
 	public GameObject passPrefab;
 	public GameObject oneWayPrefab;
+	public GameObject gridLinePrefab;
 
 	public int sizeX, sizeY;
 	public int tileScaleX, tileScaleY;
@@ -18,13 +19,34 @@ public class Grid : MonoBehaviour {
 
 		for (int x = 0; x < sizeX; x++) {
 			for (int y = 0; y < sizeY; y++) {
-				GameObject gObject = Instantiate(reflectPrefab) as GameObject;
-				Transform trans = gObject.GetComponent<Transform>();
+				// Make tile and make the grid its parent
+				GameObject tile = Instantiate(reflectPrefab) as GameObject;
+				Transform trans = tile.GetComponent<Transform>();
 				trans.parent = transform;
 				trans.localScale = new Vector3(tileScaleX, tileScaleY, 1);
 				trans.position = new Vector3(x * tileScaleX, y * tileScaleY, 1);
-				tiles[x, y] = gObject.GetComponent<Tile>();
+				tiles[x, y] = tile.GetComponent<Tile>();
+
 			}
+		}
+
+		// Draw grid lines
+		for (int x = 0; x <= sizeX; x++) {
+				GameObject gridLine = Instantiate(gridLinePrefab) as GameObject;
+				LineRenderer lr = gridLine.GetComponent<LineRenderer>();
+				lr.SetPosition(0, new Vector3(x - 0.5f * tileScaleX, -0.5f * tileScaleY, 1));
+				lr.SetPosition(1, new Vector3(x - 0.5f * tileScaleX, (sizeY - 0.5f) * tileScaleY, 1));
+				Transform trans = gridLine.GetComponent<Transform>();
+				trans.parent = transform;
+		}
+
+		for (int y = 0; y <= sizeY; y++) {
+				GameObject gridLine = Instantiate(gridLinePrefab) as GameObject;
+				LineRenderer lr = gridLine.GetComponent<LineRenderer>();
+				lr.SetPosition(0, new Vector3(-0.5f * tileScaleX, y - 0.5f * tileScaleY, 1));
+				lr.SetPosition(1, new Vector3((sizeX - 0.5f) * tileScaleX, y - 0.5f * tileScaleY));
+				Transform trans = gridLine.GetComponent<Transform>();
+				trans.parent = transform;
 		}
 	}
 
