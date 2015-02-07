65c260563f553c0ec52c1e687399ca642cb08f5c
Jeffrey Tao <jat2164@columbia.edu>
2015-02-07 01:51:23 -0500

    Using 4-space indents, starting sequencer

---
+++ Assets/Scripts/Sequencer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sequencer : MonoBehaviour {
+
+	public TextAsset sequence;
+
+	Frame[] measure;
+
+	void fromFile() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
