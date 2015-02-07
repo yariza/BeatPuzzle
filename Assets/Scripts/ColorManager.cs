5b5dc688f7f8c045b8214ea9dcd1a86d017ed633
Yujin Ariza <yujin.ariza@gmail.com>
2015-02-07 11:35:39 -0500

    marginally better colors and fixed oneway texture

--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -7,10 +7,10 @@ public class ColorManager : Singleton<ColorManager> {
 
     public Color GetColorForIndex(int index) {
         switch (index) {
-            case 0: return Color.cyan;
-            case 1: return Color.red;
-            case 2: return Color.green;
-            case 3: return Color.yellow;
+            case 0: return new Color(  0.0f/255, 21.0f/255, 255.0f/255);
+            case 1: return new Color(212.0f/255, 34.0f/255, 232.0f/255);
+            case 2: return new Color(255.0f/255, 90.0f/255, 55.0f/255);
+            case 3: return new Color(61.0f/255,  255.0f/255,48.0f/255);
             default: return Color.white;
         }
     }
