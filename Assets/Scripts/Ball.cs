1591b521fe83b58c76884b86fbb1fa3a86a12a5f
Yujin Ariza <yujin.ariza@gmail.com>
2015-02-07 02:27:02 -0500

    ball script

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public int x, y;
+    public int velX, velY;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
