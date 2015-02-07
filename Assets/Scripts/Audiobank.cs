0b14a54a8ef40d1c903db608330c6cc9404a37d4
Yujin Ariza <yujin.ariza@gmail.com>
2015-02-07 01:09:24 -0500

    audiobank implementation

---
+++ Assets/Scripts/Audiobank.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Audiobank : Singleton<Audiobank> {
+
+    public Instrument[] instruments;
+    public AudioClip bgLoop;
+
+    public int GetFrameIndex(int numFrames) {
+        return (int)((1.0 * audio.timeSamples) * numFrames / audio.clip.samples);
+    }
+
+    public void PlayHit(int frameIndex, int numFrames, int instrIndex, int soundIndex) {
+        // sound index is which sound within Instrument to play (count hits played so far)
+        double time = GetNextDSPTime(frameIndex, numFrames);
+        instruments[instrIndex].PlayHit(time, soundIndex);
+    }
+
+    private double GetNextDSPTime(int frameIndex, int numFrames) {
+        double audioLength = (1.0 * audio.clip.samples) / audio.clip.frequency;
+        double currentPosition = (1.0 * audio.timeSamples) * audioLength / audio.clip.samples;
+        double framePosition = (1.0 * frameIndex) * audioLength / numFrames;
+
+        if (currentPosition < framePosition) {
+            // it's still in the future
+            return AudioSettings.dspTime + (framePosition - currentPosition);
+        }
+        else {
+            // loop back to start
+            return AudioSettings.dspTime + (audioLength - currentPosition) + framePosition;
+        }
+    }
+
+    void Start () {
+        if (audio == null) {
+            gameObject.AddComponent<AudioSource>();
+        }
+        audio.clip = bgLoop;
+        audio.loop = true;
+
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Assets/Scripts/Instrument.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Instrument : MonoBehaviour {
+
+    public AudioClip[] sounds;
+
+    private int count;
+    private AudioSource[] sources;
+
+    public void PlayHit(double time, int index) {
+        AudioSource source = sources[index];
+        source.PlayScheduled(time);
+    }
+
+    // Use this for initialization
+    void Start () {
+        sources = new AudioSource[sounds.Length];
+        for(int i=0; i<sounds.Length; i++) {
+            AudioClip clip = sounds[i];
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            source.clip = clip;
+            sources[i] = source;
+        }
+    }
+    
+    // Update is called once per frame
+    void Update () {
+    
+    }
+}
